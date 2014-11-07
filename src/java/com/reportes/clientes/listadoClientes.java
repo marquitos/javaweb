a4d3f0a6d8340fe9a0ba5dac5caade224f7ccbdc
Marquitos <seguroestaaca@outlook.com>
2014-11-07 20:25:15 -0300

    reporte de jubilados

--- src/java/com/reportes/clientes/listadoClientes.java
+++ src/java/com/reportes/clientes/listadoClientes.java
@@ -34,7 +34,7 @@ public class listadoClientes extends HttpServlet {
      * methods.
      *
      * @param request servlet request
-     * @param response servlet response
+     * @param response servlet response  
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
